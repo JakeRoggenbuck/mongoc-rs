cf94bd88f4a40959448a9ad3b2a8e85e265bc736
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-08 19:50:44 -0700

    Use actual arg parse

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use mongodb::{
     options::{ClientOptions, FindOptions},
     Client,
 };
+use std::process;
+use std::env;
+use structopt::StructOpt;
+
 
 trait Finder {}
 
@@ -11,14 +15,25 @@ struct Database {}
 
 impl Finder for Database {}
 
-fn version() {}
+#[derive(Debug, StructOpt)]
+#[structopt(name = "mongoc-rs", about = "Quickly view your mongodb")]
+struct Opt {
+    /// The port mongodb client will use
+    #[structopt(long = "port", short = "p", default_value = "27017")]
+    port: u8,
 
-fn usage() {}
-
-fn parse_args() {}
+    /// The location mongodb is at
+    #[structopt(long = "location", short = "l", default_value = "localhost")]
+    location: String,
+}
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+
+    client_options.app_name = Some("mongoc-rs".to_string());
+    let client = Client::with_options(client_options)?;
+
+    Opt::from_args();
     Ok(())
 }
