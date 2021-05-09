2628b4ef54cc93931cd5fac5f158037f0c99e7a8
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-08 23:58:54 -0700

    Add printing features

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,55 @@
+use futures::executor::block_on;
 use futures::stream::StreamExt;
 use mongodb::{
-    error::Error,
     bson::{doc, Bson, Document},
+    error::Error,
     options::{ClientOptions, FindOptions},
-    Client, Database, Collection
+    Client, Collection, Database,
 };
 use structopt::StructOpt;
-use futures::executor::block_on;
 
-
-async fn list_databases(client: Client) -> Result<Vec<String>, Error> {
+async fn list_databases(client: &Client) -> Result<Vec<String>, Error> {
     let databases: Result<Vec<String>, Error> = client.list_database_names(None, None).await;
     return databases;
 }
 
-async fn list_collections(database: Database) -> Result<Vec<String>, Error> {
+async fn list_collections(database: &Database) -> Result<Vec<String>, Error> {
     let collections: Result<Vec<String>, Error> = database.list_collection_names(None).await;
     return collections;
+}
+
+fn enumerate_print(items: Vec<String>) {
+    let mut index: u32 = 10;
+    for item in items {
+        println!("{}:\t{}", std::char::from_digit(index, 16).unwrap(), item);
+        index += 1;
+    }
+}
+
+fn letter_print(items: Vec<String>) {
+    let mut index: u32 = 0;
+    for item in items {
+        println!("{}:\t{}", index, item);
+        index += 1;
+    }
+}
+
+fn normal_print(items: Vec<String>) {
+    for item in items {
+        println!("{}", item);
+    }
+}
+
+fn print(items: Vec<String>, enumerate: bool, numeric: bool) {
+    if enumerate {
+        if numeric {
+            enumerate_print(items);
+        } else {
+            letter_print(items);
+        }
+    } else {
+        normal_print(items);
+    }
 }
 
 #[derive(Debug, StructOpt)]
@@ -49,16 +82,23 @@ async fn main() -> mongodb::error::Result<()> {
     let mut client_options = ClientOptions::parse(client_url).await?;
 
     client_options.app_name = Some("mongoc-rs".to_string());
+
     let client = Client::with_options(client_options)?;
+    match list_databases(&client).await {
+        Ok(databases) => {
+            println!("\nDatabases: ");
+            print(databases, true, true);
+        }
+        Err(e) => println!("{}", e),
+    }
 
     let db = client.database("mydb");
-    match list_collections(db).await {
+    match list_collections(&db).await {
         Ok(collections) => {
-            for c in collections {
-                println!("{}", c);
-            }
-        },
-        Err(e) => println!("{}", e)
+            println!("\nCollections: ");
+            print(collections, true, false);
+        }
+        Err(e) => println!("{}", e),
     }
 
     Ok(())
