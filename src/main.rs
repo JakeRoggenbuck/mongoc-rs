b95a8d05303074959f8f8a3bde64270f30ead1b9
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-10 13:33:13 -0700

    Add interface for entering collections and dbs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use futures::stream::StreamExt;
+use std::io::{stdin, stdout, Write};
 use mongodb::{
-    bson::{Bson, Document},
+    bson::Document,
     error::Error,
-    options::{ClientOptions},
+    options::ClientOptions,
     Client, Database,
 };
 use structopt::StructOpt;
@@ -17,7 +18,7 @@ async fn list_collections(database: &Database) -> Result<Vec<String>, Error> {
     return collections;
 }
 
-fn enumerate_print(items: Vec<String>) {
+fn letter_print(items: &Vec<String>) {
     let mut index: u32 = 10;
     for item in items {
         println!("{}:\t{}", std::char::from_digit(index, 16).unwrap(), item);
@@ -25,7 +26,7 @@ fn enumerate_print(items: Vec<String>) {
     }
 }
 
-fn letter_print(items: Vec<String>) {
+fn enumerate_print(items: &Vec<String>) {
     let mut index: u32 = 0;
     for item in items {
         println!("{}:\t{}", index, item);
@@ -33,13 +34,13 @@ fn letter_print(items: Vec<String>) {
     }
 }
 
-fn normal_print(items: Vec<String>) {
+fn normal_print(items: &Vec<String>) {
     for item in items {
         println!("{}", item);
     }
 }
 
-fn print(items: Vec<String>, enumerate: bool, numeric: bool) {
+fn print(items: &Vec<String>, enumerate: bool, numeric: bool) {
     if enumerate {
         if numeric {
             enumerate_print(items);
@@ -54,8 +55,6 @@ fn print(items: Vec<String>, enumerate: bool, numeric: bool) {
 #[derive(Debug, StructOpt)]
 #[structopt(name = "mongoc-rs", about = "Quickly view your mongodb")]
 struct Opt {
-    item: Option<String>,
-
     /// The port mongodb client will use
     #[structopt(long = "port", short = "p", default_value = "27017")]
     port: u32,
@@ -72,6 +71,58 @@ fn make_url(port: u32, location: String) -> String {
     return "mongodb://".to_owned() + &location + ":" + &port.to_string();
 }
 
+async fn show_databases(client: &Client) -> Vec<String> {
+    let databases = list_databases(&client).await.unwrap();
+    print(&databases, true, true);
+    return databases;
+}
+
+async fn show_collections(
+    client: &Client,
+    databases: &Vec<String>,
+    database_alias: String,
+) -> (Database, Vec<String>) {
+    let index: usize = database_alias.parse().unwrap();
+    let database = client.database(&databases[index]);
+    let collections = list_collections(&database).await.unwrap();
+    print(&collections, true, false);
+    return (database, collections);
+}
+
+async fn show_documents(
+    database: &Database,
+    collections: &Vec<String>,
+    collection_alias: String,
+) -> mongodb::error::Result<()> {
+    let collection_alias_chars: Vec<char> = collection_alias.chars().collect();
+    let collection_alias_char: char = collection_alias_chars[0];
+    let index: usize = collection_alias_char as usize - 97;
+
+    let collection = database.collection::<Document>(&collections[index]);
+    let mut cursor = collection.find(None, None).await?;
+
+    while let Some(result) = cursor.next().await {
+        match result {
+            Ok(document) => {
+                println!("{}", document);
+            }
+            Err(e) => return Err(e.into()),
+        }
+    }
+
+    Ok(())
+}
+
+fn get_alias() -> String {
+    let mut alias = String::new();
+    print!("Enter an alias: ");
+    let _flushed = stdout().flush();
+    stdin().read_line(&mut alias).unwrap();
+    // Remove the trailing newline
+    alias.pop();
+    return alias;
+}
+
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let args = Opt::from_args();
@@ -85,39 +136,14 @@ async fn main() -> mongodb::error::Result<()> {
     client_options.app_name = Some("mongoc-rs".to_string());
 
     let client = Client::with_options(client_options)?;
-    match list_databases(&client).await {
-        Ok(databases) => {
-            println!("\nDatabases: ");
-            print(databases, true, true);
-        }
-        Err(e) => println!("{}", e),
-    }
 
-    let database_name = "mydb";
-    let db = client.database(database_name);
-    match list_collections(&db).await {
-        Ok(collections) => {
-            println!("\nCollections: ");
-            print(collections, true, false);
-        }
-        Err(e) => println!("{}", e),
-    }
+    let databases: Vec<String> = show_databases(&client).await;
+    let database_alias = get_alias();
 
-    let collection_name = "books";
-    let collection = db.collection::<Document>(collection_name);
-    let mut cursor = collection.find(None, None).await?;
+    let (database, collections) = show_collections(&client, &databases, database_alias).await;
+    let collection_alias = get_alias();
 
-    println!("\nDocuments: ");
-    while let Some(result) = cursor.next().await {
-        match result {
-            Ok(document) => {
-                let title = document.get("title").and_then(Bson::as_str);
-                let author = document.get("author").and_then(Bson::as_str);
-                println!("title: {:?}, article: {:?}", title, author);
-            }
-            Err(e) => return Err(e.into()),
-        }
-    }
+    let _res = show_documents(&database, &collections, collection_alias).await;
 
     Ok(())
 }
